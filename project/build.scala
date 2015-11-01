6f133cf05d1a3f0ade583064d62fd76061bc1d18
Randy <runger@gravity.com>
2015-11-01 07:50:54 -0800

    moved to single Build.scala

--- project/build.scala
+++ project/build.scala
@@ -4,13 +4,26 @@ import org.scalatra.sbt._
 import org.scalatra.sbt.PluginKeys._
 import com.mojolly.scalate.ScalatePlugin._
 import ScalateKeys._
+//import sbtassembly.AssemblyKeys._
+import sbtassembly.AssemblyPlugin.autoImport._
 
 object ExtrBuild extends Build {
-  val Organization = "org.runger"
+  val Organization = "org.runger.extrplayr"
   val Name = "ExtrPlayer"
-  val Version = "0.1.0-SNAPSHOT"
+  val Version = "0.1.1"
   val ScalaVersion = "2.11.6"
   val ScalatraVersion = "2.4.0-RC2-2"
+  val jettyVersion = "9.3.5.v20151012"
+
+//  lazy val commonSettings = Seq(
+//    version := "0.1-SNAPSHOT",
+//    organization := "com.example",
+//    scalaVersion := "2.10.1"
+//  )
+
+  test in assembly := {}
+
+  mainClass in assembly := Some("org.runger.extrplayr.WebServerRunner")
 
   lazy val project = Project (
     "ExtrPlayer",
@@ -29,8 +42,11 @@ object ExtrBuild extends Build {
         ,"org.scalatra" %% "scalatra" % ScalatraVersion
         ,"org.scalatra" %% "scalatra-scalate" % ScalatraVersion
         ,"org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test"
+        ,"org.eclipse.jetty" % "jetty-util" % jettyVersion
+        ,"org.eclipse.jetty" % "jetty-webapp" % jettyVersion
+        ,"org.eclipse.jetty" % "jetty-server" % jettyVersion
+        ,"org.eclipse.jetty" % "jetty-servlet" % jettyVersion
         ,"ch.qos.logback" % "logback-classic" % "1.1.2" % "runtime"
-        ,"org.eclipse.jetty" % "jetty-webapp" % "9.2.10.v20150310" % "container"
         ,"javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided"
         ,"commons-net" % "commons-net" % "2.0"
         ,"com.typesafe.akka" % "akka-actor_2.11" % "2.3.11"
@@ -52,3 +68,23 @@ object ExtrBuild extends Build {
     )
   )
 }
+
+
+/*
+  "org.scala-lang" % "scala-library" % "2.11.6"
+  ,"org.scalatra" %% "scalatra" % "2.4.0-RC2-2"
+  ,"org.scalatra" %% "scalatra-scalate" % "2.4.0-RC2-2"
+  ,"commons-net" % "commons-net" % "2.0"
+  ,"com.typesafe.akka" % "akka-actor_2.11" % "2.3.11"
+  ,"com.typesafe.akka" % "akka-testkit_2.11" % "2.3.11" % "test"
+  ,"org.scala-lang.modules" %% "scala-xml" % "1.0.3"
+  ,"org.mortbay.jetty" % "jetty-util" % "6.1.26" % "compile"
+  ,"org.mortbay.jetty" % "jetty" % "6.1.26" % "compile"
+  ,"org.eclipse.jetty" % "jetty-server" % "9.3.4.RC0"
+  ,"org.eclipse.jetty" % "jetty-webapp" % "9.3.4.RC0"
+//  ,"org.eclipse.jetty" % "jetty-server" % "7.6.8.v20121106"
+//  ,"org.eclipse.jetty" % "jetty-server" % "9.3.3.v20150827"
+//  ,"org.eclipse.jetty" %% "jetty-webapp" % "8.1.8.v20121106" % "container"
+//  ,"org.eclipse.jetty.orbit" %% "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
+
+ */
